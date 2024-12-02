cd6a8768aeb64e52286e689d25c4ca4b869a91f6
ryxxi <opaleyeoluwaleke@gmail.com>
2024-12-02 14:59:26 +0100

    Phase Gate

--- phaseGateOne/taskthree.py
+++ phaseGateOne/taskthree.py
@@ -1,1 +1,12 @@
-integer1
+integers = []
+count = 0
+
+while count < 3:
+    integer = input("Enter an integer: ")
+    if integer.isdigit():
+        integers.append(int(integer))
+        count += 1
+    else:
+        print("Invalid input")
+
+print(*sorted(integers), sep=", ")
